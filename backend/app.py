a34d246824ab4593f5cf21e0c648f429e551e486
Ishwor Subedi <ishworsubedi13@gmail.com>
2025-12-11 21:03:17 +0200

    file edited.

--- backend/app.py
+++ backend/app.py
@@ -80,7 +80,7 @@ def create_app():
         return redirect(url_for("admin_login"))
 
     return app
-
+app = create_app()
 if __name__ == "__main__":
-    create_app().run(host="127.0.0.1", port=5000, debug=True)
+    app.run(host="127.0.0.1", port=5000, debug=True)
 
